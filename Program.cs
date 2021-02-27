bbc0eb6445bf9f821b3596e321d13b7b326bee19
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-26 18:45:18 -0700

    Se implemento la opción para que el jugar elija si desea o no una carta.

--- Program.cs
+++ Program.cs
@@ -21,10 +21,30 @@ namespace BlackJack_SimpleGame
 
         }
 
+        /// <summary>
+        /// Preguntar al usuario si desea un carta.
+        /// </summary>
+        /// <returns>La decision tomada: Y - Desea cartas, N - no desea cartas</returns>
+        static string SolicitarCartas()
+        {
+            // Y: Yes
+            // N: No
+            Console.WriteLine("¿Deseas tomar una carta?");
+            Console.WriteLine("Y = Deseo una carta");
+            Console.WriteLine("N = No deseo una carta");
+
+             string option = Convert.ToString(Console.ReadLine());
+             return option;
+        }
+
         static void Main(string[] args)
         {
             // TODO: El jugador comienza con un manojo de cartas vacio
             int cartasJugador = PrimerJuego();
+
+            // TODO: Preguntar al jugar si desea quedarse con las cartas que tiene, o si solicita una carta.
+            string option = SolicitarCartas();
+            Console.WriteLine("Opción seleccionada: " + option);
         }
     }
 }
