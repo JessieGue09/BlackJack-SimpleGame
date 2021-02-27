9b45b029ff8ba66c2774b84d5bbce2bd35ede8c5
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-26 18:49:21 -0700

    Se agrega la baraja de forma aleatorea, se muestra según lo que el jugador decida el número de cartas + el número de cartas actual.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@ namespace BlackJack_SimpleGame
     class Program
     {
         /// <summary>
+        /// Creación de las cartas de la baraja (1 al 11, tomando al A como 11).
+        /// </summary>
+        /// <returns>La carta aleatorea generada (option)</returns>
+        static int Cartas()
+        {
+            Random rnd = new Random();
+            // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11.
+            int option = rnd.Next(1,12);
+            return option;
+        }
+
+        /// <summary>
         /// Le damos la bienvenida al jugador y mostramos que comienza con 0 cartas.
         /// </summary>
         static int PrimerJuego()
@@ -17,7 +29,6 @@ namespace BlackJack_SimpleGame
 
             
             return cartasJugador;
-            
 
         }
 
@@ -37,6 +48,32 @@ namespace BlackJack_SimpleGame
              return option;
         }
 
+        /// <summary>
+        /// Creación de condicionales (if/else if) para determinar según la opción que eligió el jugar si se le la una carta o no.
+        /// Se genera la variable suma, la cual suma las cartas actuales del jugador con las que se les da y muestra cuantas tiene.   
+        /// </summary>
+        /// <param name="option">Opción que el jugador eligio, si solicita cartas (Y) o no (N)</param>
+        /// <param name="rancartas">Cartas que la casa da al jugador (cartas random)</param>
+        /// <param name="cartasJugador">Numero de cartas que tiene actualmente el jugaor</param>
+        static void Tiradas(string option, int rancartas, int cartasJugador)
+        {
+            int suma = cartasJugador + rancartas;
+
+            if (option == "Y")
+            {
+                Console.WriteLine("Carta aleatorea: " +  rancartas);
+                Console.WriteLine("Su número de cartas actual es: " + suma);
+            } 
+            else if(option == "N")
+            {
+                Console.WriteLine("Su número de cartas actual es: " + cartasJugador);
+            }
+            else 
+            {
+                Console.WriteLine("Comando invalido");
+            }
+        }
+
         static void Main(string[] args)
         {
             // TODO: El jugador comienza con un manojo de cartas vacio
@@ -45,6 +82,12 @@ namespace BlackJack_SimpleGame
             // TODO: Preguntar al jugar si desea quedarse con las cartas que tiene, o si solicita una carta.
             string option = SolicitarCartas();
             Console.WriteLine("Opción seleccionada: " + option);
+
+            // Mostrar aleatoreamente una carta de la baraja.
+            int rancartas = Cartas();
+            
+            // TODO: Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
+            Tiradas(option, rancartas, cartasJugador);
         }
     }
 }
