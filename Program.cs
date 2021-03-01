99c27e4db66f1a7e8fdffe5652f7c988e746d9a0
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-03-01 10:23:52 -0700

    Creacion de condicionales para definir un ganador.

--- Program.cs
+++ Program.cs
@@ -52,19 +52,22 @@ namespace BlackJack_SimpleGame
         /// Creación de condicionales (if/else if) para determinar según la opción que eligió el jugar si se le la una carta o no.
         /// Creación de un ciclo para preguntar al usuario si desea otra carta, si es así; se genera un número aleatorio y se suma con el número de cartas del jugador.
         /// Se genera la variable random Acartas (17 - 26), el cúal será el número de la casa.
+        /// Se implementa el metodo Ganador() para definir un ganador entre la casa y el jugador.
         /// </summary>
         /// <param name="option"> Opción que el jugador eligio, si solicita cartas (Y) o no (N)</param>
         /// <param name="rancartas">Manda a llamar la varible random para generar un número aleatorio<param>
         /// <param name="cartasJugador">Manda a llamar el número de cartas que tiene el jugador </param>
         static void Tiradas(string option, int rancartas, int cartasJugador)
         {
+
             //Cartas aleatorias del AI
-                Random rnd2 = new Random();
-                // 17, 18, 19, 20, 21, 22, 23, 24, 25, 26.
-                int Acartas = rnd2.Next(16, 26);
+            Random rnd2 = new Random();
+            // 17, 18, 19, 20, 21, 22, 23, 24, 25, 26.
+            int Acartas = rnd2.Next(16, 26);
 
             while (option == "Y")
             {
+                //Cartas aleatorias del jugador. (Se generan de nuevo dentro del ciclo).
                 Random rnd = new Random();
                 // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11.
                 int rancartas2 = rnd.Next(1, 12);
@@ -75,15 +78,17 @@ namespace BlackJack_SimpleGame
                 Console.WriteLine("Carta aleatoria: " + rancartas2);
                 Console.WriteLine("Su número de cartas actual es: " + cartasJugador);
 
+                // El jugador pasa de 21, pierde automaticamente
                 if (cartasJugador > 21)
                 {
-                    Console.WriteLine("Pierdes");
                     Console.WriteLine("El número de cartas que tiene la casa es: " + Acartas);
-
-                    break;
+                    Ganar(Acartas, cartasJugador);
+                    
+                    break; // Fin del ciclo.
                 }
                 else
                 {
+                    //Se pregunta de nuevo si desea tomar otra carta.
                     Console.WriteLine("¿Deseas tomar una carta?");
                     Console.WriteLine("Y = Deseo una carta");
                     Console.WriteLine("N = No deseo una carta");
@@ -95,9 +100,59 @@ namespace BlackJack_SimpleGame
             }
             if (option == "N")
             {
+                // TODO: Mostrar los números obtenidos por el jugador y por la casa.
                 Console.WriteLine("Su número de cartas actual es: " + cartasJugador);
 
                 Console.WriteLine("El número de cartas que tiene la casa es: " + Acartas);
+
+                // Se llama al metodo ganar en caso que el jugador decida detenerse.
+                Ganar(Acartas, cartasJugador);
+            }
+        }
+
+        /// <summary>
+        /// Se implemento condicionales para definir un ganador.
+        /// </summary>
+        /// <param name="Acartas">Número de cartas que tiene la casa</param>
+        /// <param name="cartasJugador">Número de cartas que tiene el jugador.</param>
+        static void Ganar(int Acartas, int cartasJugador)
+        {
+            // El jugador gana cuando:
+            // - El jugador obtiene un 21 y la casa obtiene un número menor.
+            if (cartasJugador == 21 && Acartas < 21)
+            {
+                Console.WriteLine("¡El Jugador gana!");
+            }
+            // - La casa obtiene un número mayor a 21 y el jugador obtiene un número menor o igual que 21.
+            else if (Acartas > 21 && cartasJugador <= 21)
+            {
+                Console.WriteLine("¡El Jugador gana!");
+            }
+            // - El jugador y la casa obtienen un número menor o igual a 21, pero el jugador obtiene un número más alto.
+            else if (cartasJugador <= 21 && Acartas <= 21 && cartasJugador > Acartas)
+            {
+                Console.WriteLine("¡El Jugador gana!");
+            }
+            // - El jugador y la casa obtienen el mismo número, siendo este un número menor o igual a 21, pero el jugador lo hace con menos cartas.
+            else if (cartasJugador <= 21 && Acartas <= 21 && cartasJugador == Acartas)
+            {
+                Console.WriteLine("¡El Jugador gana!");
+            }
+            // Hay un empate cuando:
+            // - Ambos obtienen un número mayor a 21
+            else if (cartasJugador > 21 && Acartas > 21)
+            {
+                Console.WriteLine("¡Empate!");
+            }
+            // - Ambos obtienen un número menor o igual a 21, con el mismo número de cartas
+            else if (cartasJugador <= 21 && Acartas <= 21 && cartasJugador == Acartas)
+            {
+                Console.WriteLine("¡Empate!");
+            }
+            // En caso contrario a los anteriores, el jugador pierde.
+            else
+            {
+                Console.WriteLine("¡La casa gana!");
             }
         }
 
@@ -115,8 +170,11 @@ namespace BlackJack_SimpleGame
 
             // TODO: Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
             // TODO: Calcular el valor de las cartas que tiene el jugador (numérico) y mostrarlo cada vez que el jugador obtenga una carta. Considerar el valor del as como 11.
-            // Cuando el jugador se detenga, generar aleatoriamente un número entre el 17 y el 26 (incluyendo ambos números). Este será el valor de "la casa" contra el cual debe competir el número obtenido por las cartas del jugador.
+            // TODO: Cuando el jugador se detenga, generar aleatoriamente un número entre el 17 y el 26 (incluyendo ambos números). Este será el valor de "la casa" contra el cual debe competir el número obtenido por las cartas del jugador.
             Tiradas(option, rancartas, cartasJugador);
+
+            // TODO: Mostrar los números obtenidos por el jugador y por la casa.
+
 
         }
     }
