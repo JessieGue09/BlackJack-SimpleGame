bad148b5a2400cca66d05433fac4a6623128f49a
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-28 19:01:52 -0700

    Creación de un ciclo para preguntar al usuario si desea otra carta y generar un número aleatorio.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace BlackJack_SimpleGame
         {
             Random rnd = new Random();
             // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11.
-            int option = rnd.Next(1,12);
+            int option = rnd.Next(1, 12);
             return option;
         }
 
@@ -27,7 +27,7 @@ namespace BlackJack_SimpleGame
             // Mostrar al jugar con cuantas cartas comienza
             Console.WriteLine("¡Bienvenido!, has comenzado con: " + cartasJugador + " cartas.");
 
-            
+
             return cartasJugador;
 
         }
@@ -44,26 +44,48 @@ namespace BlackJack_SimpleGame
             Console.WriteLine("Y = Deseo una carta");
             Console.WriteLine("N = No deseo una carta");
 
-             string option = Convert.ToString(Console.ReadLine());
-             return option;
+            string option = Convert.ToString(Console.ReadLine());
+            return option;
         }
 
         /// <summary>
-        /// Creación de condicionales (if/else if) para determinar según la opción que eligió el jugar si se le la una carta o no.   
+        /// Creación de condicionales (if/else if) para determinar según la opción que eligió el jugar si se le la una carta o no.
+        /// Creación de un ciclo para preguntar al usuario si desea otra carta, si es así; se genera un número aleatorio y se suma con el número de cartas del jugador.
         /// </summary>
-        /// <param name="option">Opción que el jugador eligio, si solicita cartas (Y) o no (N)</param>
+        /// <param name="option"> Opción que el jugador eligio, si solicita cartas (Y) o no (N)</param>
+        /// <param name="rancartas">Manda a llamar la varible random para generar un número aleatorio<param>
+        /// <param name="cartasJugador">Manda a llamar el número de cartas que tiene el jugador </param>
         static void Tiradas(string option, int rancartas, int cartasJugador)
         {
-            if (option == "Y")
+            while (option == "Y")
             {
-                Console.WriteLine("Carta aleatorea: " +  rancartas);
-                //Console.WriteLine("Su número de cartas actual es: " + suma);
-            } 
-            else if(option == "N")
+                Random rnd = new Random();
+                // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11.
+                int rancartas2 = rnd.Next(1, 12);
+                int sum = cartasJugador + rancartas2;
+                cartasJugador = sum;
+
+                if (cartasJugador > 21)
+                {
+                    Console.WriteLine("Pierdes");
+
+                    Console.WriteLine("Carta aleatoria: " + rancartas2);
+                    Console.WriteLine("Su número de cartas actual es: " + cartasJugador);
+                    Console.WriteLine("¿Deseas tomar una carta?");
+                    Console.WriteLine("Y = Deseo una carta");
+                    Console.WriteLine("N = No deseo una carta");
+
+                    option = Convert.ToString(Console.ReadLine());
+
+                }
+                break;
+
+            }
+            if (option == "N")
             {
                 Console.WriteLine("Su número de cartas actual es: " + cartasJugador);
             }
-            else 
+            else
             {
                 Console.WriteLine("Comando invalido");
             }
@@ -74,7 +96,7 @@ namespace BlackJack_SimpleGame
         /// </summary>
         /// <param name="rancartas">Cartas que la casa da al jugador (cartas random)</param>
         /// <param name="cartasJugador">Numero de cartas que tiene actualmente el jugador</param>
-        static void SumaCartas (int rancartas, int cartasJugador)
+        static void SumaCartas(int rancartas, int cartasJugador)
         {
             int suma = cartasJugador + rancartas;
 
@@ -93,13 +115,13 @@ namespace BlackJack_SimpleGame
 
             // Mostrar aleatoreamente una carta de la baraja.
             int rancartas = Cartas();
-            
+
             // TODO: Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
+            // TODO: Calcular el valor de las cartas que tiene el jugador (numérico) y mostrarlo cada vez que el jugador obtenga una carta. Considerar el valor del as como 11.
             Tiradas(option, rancartas, cartasJugador);
 
-            // TODO: Calcular el valor de las cartas que tiene el jugador (numérico) y mostrarlo cada vez que el jugador obtenga una carta. Considerar el valor del as como 11.
-            //Comentario pa ver en que rama estamos u3u.
-            SumaCartas(rancartas, cartasJugador);
+
+            //SumaCartas(rancartas, cartasJugador);
         }
     }
 }
