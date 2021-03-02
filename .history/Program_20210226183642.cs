75ba81a7c3725e51fde629ab69b880627a3b19ce
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-03-01 19:51:37 -0700

    Creación del archivo README

---
+++ .history/Program_20210226183642.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BlackJack_SimpleGame
+{
+    class Program
+    {
+        /// <summary>
+        /// Le damos la bienvenida al jugador y mostramos que comienza con 0 cartas.
+        /// </summary>
+        static int PrimerJuego()
+        {
+            // El jugador comienza con 0 cartas; 
+            int cartasJugador = 0;
+
+            // Mostrar al jugar con cuantas cartas comienza
+            Console.WriteLine("¡Bienvenido!, has comenzado con: " + cartasJugador + " cartas.");
+
+            
+            return cartasJugador;
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO: El jugador comienza con un manojo de cartas vacio
+            int cartasJugador = PrimerJuego();
+        }
+    }
+}
